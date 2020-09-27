ddd49c4b30f92980c8ece82ad3271d73b419322a
sihem.mohamed <sihem.mohamed@sofrecom.com>
2020-09-27 11:21:41 +0100

    Initial Commit

---
+++ Me/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Me
+//
+//  Created by Sofrecom2 on 9/27/20.
+//  Copyright © 2020 Simo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MeTests/MeTests.swift
@@ -0,0 +1,34 @@
+//
+//  MeTests.swift
+//  MeTests
+//
+//  Created by Sofrecom2 on 9/27/20.
+//  Copyright © 2020 Simo. All rights reserved.
+//
+
+import XCTest
+@testable import Me
+
+class MeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
